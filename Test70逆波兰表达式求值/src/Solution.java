c46bf2f6b21807a808b8c8ccaeb6e90932cef3f8
飞 <1752840757@qq.com>
2020-04-30 22:17:58 +0800

    逆波兰表达式求职

---
+++ Test70逆波兰表达式求值/src/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public static int evalRPN(String[] tokens) {
+        int[] numStack = new int[tokens.length / 2 + 1];
+        int index = 0;
+        for (String s : tokens) {
+            switch (s) {
+                case "+":
+                    numStack[index - 2] += numStack[--index];
+                    break;
+                case "-":
+                    numStack[index - 2] -= numStack[--index];
+                    break;
+                case "*":
+                    numStack[index - 2] *= numStack[--index];
+                    break;
+                case "/":
+                    numStack[index - 2] /= numStack[--index];
+                    break;
+                default:
+                    // numStack[index++] = Integer.valueOf(s);
+                    //valueOf改为parseInt，减少自动拆箱装箱操作
+                    numStack[index++] = Integer.parseInt(s);
+                    break;
+            }
+        }
+        return numStack[0];
+    }
+}
